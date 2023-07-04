26688a4c94f601e6062d6541662b23c53df0d54b
milana-maksimovic <115220073+milana-maksimovic@users.noreply.github.com>
2023-07-04 11:29:15 +0200

    Revert "frontend"
    This reverts commit 8a3564a4e86feb578026e7c173248cb8de335a85.

--- erp-front/src/utility/DateTimeUtility.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment'
-
-const DateTimeUtility = {
-    formatDate: function(date) {
-        return moment(date).format("Do MMMM YYYY HH:mm:ss")
-    }
-}
-
-export default DateTimeUtility
